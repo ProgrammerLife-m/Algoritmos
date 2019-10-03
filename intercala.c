63b54e5137228015677c3e8b5c6370c06f24ba2a
ProgrammerLife-m <programmerlife.m@gmail.com>
2019-10-03 19:51:17 -0400

    Mesclando Vetores Ordenados

--- intercala.c
+++ intercala.c
@@ -1,66 +1,73 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void resultado(int n, int m, int p, int *v, int *u, int *w);
+int *mesclar(int tam1, int vetor1[], int tam2, int vetor2[]);
 
 void main(void)
 {
-	int teste, i, n, m;
+	int teste, i, m, n;
 
 	scanf("%d", &teste);
 	while (teste > 0)
 	{
-		scanf("%d", &n);
-		int v[n];
-		for (i = 0; i < n; i++)
-		{
-			scanf("%d", &v[i]);
-		}
-
 		scanf("%d", &m);
-		int u[m];
+		
+		int vetor1[m];
 		for (i = 0; i < m; i++)
 		{
-			scanf("%d", &u[i]);
+			scanf("%d", &vetor1[i]);
 		}
 
-		int p = m + n, vet[p];
-		resultado(n, m, p, v, u, vet);
-		for (i = 0; i < p; i++)
+		scanf("%d", &n);
+		
+		int vetor2[n];
+		for (i = 0; i < n; i++)
+		{
+			scanf("%d", &vetor2[i]);
+		}
+
+		int *vet;
+		vet = mesclar(m, vetor1, n, vetor2);
+
+		for (i = 0; i < (m+n); i++)
 		{
 			printf("%d ", vet[i]);
 		}
-
 		teste--;
+		free(vet);
 	}
 }
-void resultado(int n, int m, int p, int *v, int *u, int *w)
+int *mesclar(int tam1, int vetor1[], int tam2, int vetor2[])
 {
 	
 	int i, contV = 0, contU = 0, aux = 1;
+	int *vet;
+	vet = malloc((tam1+tam2)*sizeof(int));
 
-	for (i = 0; i < p; i++)
+	for (i = 0; i < (tam1+tam2); i++)
 	{
-		if (u[contU] >= v[contV] && aux == 1)
+		if (vetor2[contU] >= vetor1[contV] && aux == 1)
 		{
-			w[i] = v[contV];
+			vet[i] = vetor1[contV];
 			contV++;
 		}
-		else if (aux == 1)
+		else if(aux == 1)
 		{
-			w[i] = u[contU];
+			vet[i] = vetor2[contU];
 			contU++;
 		}
-		if (contV == n + 1 && contU < m)
+		if (contV == tam1 + 1 && contU < tam2)
 		{
-			w[i] = u[contU];
+			vet[i] = vetor2[contU];
 			contU++;
 			aux = 0;
 		}
-		else if (contU == m + 1 && contV < n)
+		else if (contU == tam2 + 1 && contV < tam1)
 		{
-			w[i] = v[contV];
+			vet[i] = vetor1[contV];
 			contV++;
 			aux = 0;
 		}
 	}
+	return vet;
 }
